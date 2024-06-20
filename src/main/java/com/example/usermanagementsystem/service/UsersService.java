711af754f057dc6cfc6b7e7d87cd2cf4dcf6e683
Paulo Raimundo <pauloraimundo@paulo.raimundo>
2024-06-19 21:58:39 -0300

    Implementando função de criar usuário.

--- src/main/java/com/example/usermanagementsystem/service/UsersService.java
+++ src/main/java/com/example/usermanagementsystem/service/UsersService.java
@@ -1,4 +1,18 @@
 package com.example.usermanagementsystem.service;
 
-public class UsersService {
+import com.example.usermanagementsystem.entity.Users;
+import com.example.usermanagementsystem.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UsersService implements UsersServiceInterface {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Override
+    public Users createUsers(Users users) {
+        return usersRepository.save(users);
+    }
 }
--- src/main/java/com/example/usermanagementsystem/service/UsersServiceInterface.java
+++ src/main/java/com/example/usermanagementsystem/service/UsersServiceInterface.java
@@ -1,4 +1,11 @@
 package com.example.usermanagementsystem.service;
 
+import com.example.usermanagementsystem.entity.Users;
+
+import java.util.List;
+
 public interface UsersServiceInterface {
+
+    Users createUsers(Users users);
+
 }
