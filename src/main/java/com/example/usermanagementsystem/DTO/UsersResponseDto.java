56ca2d5f13c341fd55a0f4b20cdb6dacf0ecd9b2
Paulo Raimundo <pauloraimundo@paulo.raimundo>
2024-06-26 21:10:34 -0300

    Criando DTO para tratar a resposta do departamento no endpoint de busca de usuário pelo id.

---
+++ src/main/java/com/example/usermanagementsystem/DTO/UsersResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.usermanagementsystem.DTO;
+
+public class UsersResponseDto {
+}
---
+++ src/main/java/com/example/usermanagementsystem/security/WebConfig.java
@@ -0,0 +1,4 @@
+package com.example.usermanagementsystem.security;
+
+public class WebConfig {
+}
